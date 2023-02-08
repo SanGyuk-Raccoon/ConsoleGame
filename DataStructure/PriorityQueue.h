4f234721e352fb16669f1a3e20e1324cf749fbda
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2023-02-08 23:23:49 +0900

    add HashTable.h (#13)
    Co-authored-by: ebs.kim <ebs.kim@samsung.com>

--- DataStructure/PriorityQueue.h
+++ DataStructure/PriorityQueue.h
@@ -2,9 +2,6 @@
 #include "Allocator.h"
 #include <memory.h>
 
-// PQ를 만들어보자.
-// Heap을 구현해본적이 없어서 해보고 싶었던 자료구조.
-// ToDo : Custom Compare Function 설정할 수 있도록 하자.
 template<typename T, typename _Allocator = DefaultAllocator<T>>
 class PriorityQueue {
 private:
